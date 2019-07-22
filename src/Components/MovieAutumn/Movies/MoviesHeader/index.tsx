1bdb2c7404603c096aa56888d1256503550fef52
charliechauri <carlos.echauri@gmail.com>
2019-07-22 02:17:03 -0500

    feat Add header component for movies

---
+++ src/Components/MovieAutumn/Movies/MoviesHeader/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MoviesHeader() {
+  return(
+    <h2>
+      <mark>
+        Movie Autumn 2019 <span role="img" aria-label="film">🎬</span>
+        <span role="img" aria-label="party popper">🎉</span>
+      </mark>
+    </h2>
+  );
+}
+
+export default MoviesHeader;
--- src/Components/MovieAutumn/Movies/index.tsx
+++ src/Components/MovieAutumn/Movies/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '../../Button';
 import MovieList from './MovieList';
+import MoviesHeader from './MoviesHeader';
 import MoviesDescription from './MoviesDescription';
 import movies from './movies';
 import './index.css';
@@ -8,12 +9,8 @@ import './index.css';
 function Movies() {
   return (
     <div className="movies">
-      <h2>
-        <mark>
-          Movie Autumn 2019 <span role="img" aria-label="film">🎬</span>
-          <span role="img" aria-label="party popper">🎉</span>
-        </mark>
-      </h2>
+
+      <MoviesHeader />
 
       <MoviesDescription />
 
