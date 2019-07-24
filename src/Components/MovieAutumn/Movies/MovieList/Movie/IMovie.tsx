97e176afd9fe4577fc02f474c4af83d1ca9d630d
charliechauri <carlos.echauri@gmail.com>
2019-07-23 23:42:21 -0500

    feat Add new properties for movie interface
    - Update using of IMovie with mock data

--- src/Components/MovieAutumn/Movies/MovieList/Movie/IMovie.tsx
+++ src/Components/MovieAutumn/Movies/MovieList/Movie/IMovie.tsx
@@ -1,3 +1,10 @@
 export default interface IMovie {
   name: string;
+  sinopsis: string;
+  genre: string;
+  year: number;
+  duration: string;
+  coverUrl: string;
+  trailerUrl: string;
+  starsOnIMDB: number;
 };
