c0dfb8f3a5e070fdbe3b81e6f4aa43f968da1752
charliechauri <carlos.echauri@gmail.com>
2019-07-23 23:54:56 -0500

    feat Add new properties for IMovie interface
    - Change extension to `.ts`
    - Update movies with mock data in order to comply the new schema

--- src/Components/MovieAutumn/Movies/MovieList/Movie/IMovie.ts
+++ src/Components/MovieAutumn/Movies/MovieList/Movie/IMovie.ts
@@ -1,10 +1,10 @@
+import IImdbInfo  from './IImdbInfo' ;
+
 export default interface IMovie {
   name: string;
   sinopsis: string;
   genre: string;
   year: number;
-  duration: string;
-  coverUrl: string;
-  trailerUrl: string;
-  starsOnIMDB: number;
+  lenghtInMinutes: number;
+  imdbInfo: IImdbInfo;
 };
--- src/Components/MovieAutumn/Movies/movies.ts
+++ src/Components/MovieAutumn/Movies/movies.ts
@@ -3,103 +3,123 @@ import Movie from './MovieList/Movie/IMovie';
 const movies: Movie[] = [
   {
     name: 'Jerry Maguire',
-    sinopsis: 'string',
-    genre: 'string',
-    year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    sinopsis: 'When a sports agent has a moral epiphany and is fired for expressing it, he decides to put his new philosophy to the test as an independent agent with the only athlete who stays with him and his former secretary.',
+    genre: 'Comedy, Drama, Romance',
+    year: 1996,
+    lenghtInMinutes: 139,
+    imdbInfo: {
+      coverURI: 'M/MV5BYTM0ZWNmZTUtOTVkZS00MTZiLTg3M2QtZjA0Y2RmOWM1NWEyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg',
+      trailerId: 'vi878683161',
+      stars: 7.3,
+    }
   },
   {
     name: 'Fifty First Dates',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'The wedding singer',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'Aladdin',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'The Beauty and The Beast',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'The Vow',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'Serendipity',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'Ruby Sparks',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: 'About Time',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   },
   {
     name: '10 Things I Hate About You',
     sinopsis: 'string',
     genre: 'string',
     year: 1900,
-    duration: 'string',
-    coverUrl: 'string',
-    trailerUrl: 'string',
-    starsOnIMDB: 10,
+    lenghtInMinutes: 120,
+    imdbInfo: {
+      coverURI: '',
+      trailerId: '',
+      stars: 10,
+    }
   }
 ];
 
